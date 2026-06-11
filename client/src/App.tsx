12423fc49e0ec5263a7eebd2025c4e3dc0751250
tommyszalichtrabajo <tommyszalichtrabajo@gmail.com>
2026-06-11 16:03:47 -0300

    feat: setup React with Vite and Tailwind CSS

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">Saciety</h1>
+    </div>
+  )
+}
+
+export default App  
